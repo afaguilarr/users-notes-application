c69b74c19661e5cc1af6bd9b8c9a6f0403eb6a36
Andrés Felipe Aguilar Rendón <afaguilarr@unal.edu.co>
2018-03-07 21:04:00 -0500

    Path update

--- src/main/java/hello/NoteRepository.java
+++ src/main/java/hello/NoteRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.mongodb.repository.Query;
 
-@RepositoryRestResource(collectionResourceRel = "Notes", path = "Notes")
+@RepositoryRestResource(collectionResourceRel = "notes", path = "notes")
 public interface NoteRepository extends MongoRepository<Note, String> {
 	public List<Note> findByText(@Param("text") String text);
     public List<Note> findByDate(@Param("date") String date);
--- src/main/java/hello/UserRepository.java
+++ src/main/java/hello/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "Users", path = "Users")
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends MongoRepository<User, String> {
 	public User findByUsername(@Param("username") String username);
 }
